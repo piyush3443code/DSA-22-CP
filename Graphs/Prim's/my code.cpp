ef6bff4a75d7f102cc4765ab0c1aefba8444069a
piyush3443code <87921486+piyush3443code@users.noreply.github.com>
2022-09-04 09:59:49 +0530

    Update my code.cpp

--- Graphs/Prim's/my code.cpp
+++ Graphs/Prim's/my code.cpp
@@ -1,135 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> PrimsAlgo(int **edgeMat, int n,int sv){
-     //Source vertex as provided by User,
-     
-    int wt[n] = {1000000};  // Weight array storing each vertex weights.
-    wt[sv] = 0;
-    
-    vector<int>parent(n,-1);     // Parent array storing the parent of a vertex from the final edge included in MST.
-    bool visited[n] = {false};     // Visited array to mark the vertex visited.
-    
-    int count = 0;
-    
-    while(count<=n-1){
-        
-        int min = INT_MAX;
-        int idx;
-        
-        for(int i = 0;i<n;i++){
-            
-            if(!visited[i] && wt[i]<min){
-                
-                min = wt[i];
-               idx = i;
-            
-            }
-        }
-        cout<<min;
-        visited[idx] = true;
-     
-        
-        for(int i = 0;i<n;i++){
-            
-           
-            if(edgeMat[idx][i]!=0 && !visited[i]){
-                cout<<i<<" ";
-                 
-                if(edgeMat[idx][i]<wt[i])
-                {
-                    cout<<" parent changed.";
-                    wt[i] = edgeMat[idx][i];
-                    parent[i] = idx;
-                }
-                
-                
-                
-            }
-        }
-           count++;
-        
-        
-        
-        
-        
-        
-        
-    }
-    
-     for(int i = 0;i<n;i++){
-        
-        if(parent[i]==-1)
-            continue;
-    
-        if(parent[i]<i)
+int findMinVertex(int *weight, bool *visited, int n)
+{
+    int minVertex = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i] && (minVertex == -1 || weight[i] < weight[minVertex]))
         {
-            cout<<parent[i]<<" "<<i<<" "<<edgeMat[parent[i]][i]<<endl;
+            minVertex = i;
+        }
+    }
+    return minVertex;
+}
+
+void prims(int **edges, int n)
+{
+    int *parent = new int[n];
+    int *weight = new int[n];
+    bool *visited = new bool[n];
+
+    for (int i = 1; i < n; i++)
+    {
+        visited[i] = false;
+        weight[i] = INT_MAX;
+    }
+    parent[0] = -1;
+    visited[0] = false;
+    weight[0] = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        //Find min vertex
+        int minVertex = findMinVertex(weight, visited, n);
+        visited[minVertex] = true;
+        //Explore un-visited neighbours
+        for (int j = 0; j < n; j++)
+        {
+            if (edges[minVertex][j] != 0 && !visited[j])
+            {
+                if (edges[minVertex][j] < weight[j])
+                {
+                    weight[j] = edges[minVertex][j];
+                    parent[j] = minVertex;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        if (i < parent[i])
+        {
+            cout << i << " " << parent[i] << " " << weight[i] << endl;
         }
         else
         {
-            cout<<i<<" "<<parent[i]<<" "<<edgeMat[parent[i]][i]<<endl;
+            cout << parent[i] << " " << i << " " << weight[i] << endl;
         }
-         cout<<parent[i];  // To debug the code.
     }
-    
-    
-    return parent;
-    
-    
-    
-    
 }
 
 
-
-
-int main() {
-    // Write your code here
-    
-    int n,edges;
-    
-    cin>>n>>edges;
-    
-    int **edgeMat = new int*[n];
-    
-    for(int i = 0;i<n;i++){
-        
-        edgeMat[i] = new int[n];
-        
-        for(int j = 0;j<n;j++){
-            
-            edgeMat[i][j] = 0;
-            
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int **edges = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        edges[i] = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            edges[i][j] = 0;
         }
     }
-    
-    for(int i = 0;i<edges;i++){
-        
-        int f,s,wt;
-        cin>>f>>s>>wt;
-        
-        edgeMat[f][s] = wt;
-        edgeMat[s][f] = wt;
-        
-        
-    }
-    
-     
-    
-    vector<int> parent = PrimsAlgo(edgeMat,n,0);
-    
-   
-    
-    for(int i = 0;i<n;i++)
+
+    for (int i = 0; i < e; i++)
     {
-        delete [] edgeMat[i];
+        int f, s, w;
+        cin >> f >> s >> w;
+        edges[f][s] = w;
+        edges[s][f] = w;
     }
-    
-    delete [] edgeMat;
-    
-    return 0;
-    
-    
-    
+
+    prims(edges, n);
 }
